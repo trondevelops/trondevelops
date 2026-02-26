2429578bb8458105fadbf009a838687a432a827c
trondevelops <trondevelops@gmail.com>
2026-02-26 23:13:48 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// SCROLL ANIMATION
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".fade-in").forEach((el) => {
+  observer.observe(el);
+});
+
+
+// CURSOR GLOW
+
+const glow = document.createElement("div");
+glow.classList.add("cursor-glow");
+document.body.appendChild(glow);
+
+document.addEventListener("mousemove", (e) => {
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
+});
